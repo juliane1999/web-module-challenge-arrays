7914e5bb6a57ff715dbe9d14f32a4fc92fd97fba
juliane1999 <lastjuliane@gmail.com>
2021-06-09 12:29:10 -0700

    completed project all 7 tasks in Javascript

--- index.js
+++ index.js
@@ -45,9 +45,12 @@ Use the copy function below to do the following:
     2. Return a copy of the received array  
 */
 
-function copy(/*your code here*/){
-    /*your code here*/
-}    
+function copy(array){
+    const originalFlavors = [...originalFlavors];
+
+return array;  
+} 
+
 
 
 
@@ -64,9 +67,16 @@ For Example: is31Flavors(originalFlavors) will return true if your code is worki
 */
 
 
-function is31Flavors(/*your code here*/){
-   /*your code here*/
+function is31Flavors(array){
+    if((originalFlavors.length) === 31){
+    return true;
 }
+else{
+    return false;
+}
+}
+
+
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Corporate has come to you with an idea for a new flavor: Rainbow Sherbert! They think this will be a game changer. You need to modify the array to include this flavor. 
@@ -80,9 +90,10 @@ Use the addFlavor function below to do the following:
     For example: addFlavor(originalFlavors, "Rainbow Sherbert") should return the array ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla Burnt Almond"]
 */
 
+function addFlavor(array,string){
+    array.unshift(string);
 
-function addFlavor(/*your code here*/){
-   /*your code here*/
+    return array;
 }
 
 
@@ -97,10 +108,10 @@ Use the removeLastFlavor function below to do the following:
     For example: running removeLastFlavor(originalFlavors) would return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla"]
 */
 
-function removeLastFlavor(/*your code here*/){
-   /*your code here*/
+function removeLastFlavor(array,){
+   array.pop();
+return array;
 }
-
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -114,8 +125,9 @@ Use the getFlavorByIndex function below to do the following:
     For example: running getFlavorByIndex(originalFlavors, 2) would return "Black Walnut", assuming Rainbow Sherbert has been added successfully
 */
 
-function getFlavorByIndex(/*your code here*/){
-    /*your code here*/
+function getFlavorByIndex(array,number){
+    let newarray = array [number];
+    return newarray;
 }
 
 
@@ -134,8 +146,13 @@ Use the removeFlavorByName function below to do the following:
     HINT: You can use .splice() for this
 */
 
-function removeFlavorByName(/*your code here*/){
-    /*your code here*/
+function removeFlavorByName(array,string){
+    for(let i=0; i < array.length; i++){
+        if(array[i] === string){
+            array.splice (i,1);
+        }
+    }
+    return array;
 }
 
 
@@ -305,4 +322,3 @@ export default{
     getAverageWordLength,
     getRandomFlavors
 }
-
